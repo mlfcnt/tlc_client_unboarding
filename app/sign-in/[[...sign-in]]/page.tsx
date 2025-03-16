1361a1905828145c06b18c7c33d410f7fc2d2b7c
TMN <tommy.martin@adsoftware.fr>
2025-03-16 06:30:06 -0500

    fix: auth

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,11 @@ import {SignIn} from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50">
-      <SignIn />
+      <SignIn
+        unsafeMetadata={{
+          role: "sales",
+        }}
+      />
     </div>
   );
 }
