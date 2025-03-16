9271b7614806b5599fcb9e9a987db3949dcd3f8a
TMN <tommy.martin@adsoftware.fr>
2025-03-16 09:07:22 -0500

    security

--- app/dashboard/onboarding-details/page.tsx
+++ app/dashboard/onboarding-details/page.tsx
@@ -2,14 +2,22 @@
 
 import Page from "@/app/components/Page";
 import {OnboardingDetailsGrid} from "./components/OnboardingDetailsGrid";
-import {useSearchParams} from "next/navigation";
+import {redirect, useSearchParams} from "next/navigation";
 import {OnboardingStatuses} from "@/app/constants/OnboardingStatuses";
+import {useUser} from "@clerk/nextjs";
 export default function OnboardingDetailsPage() {
   const queryParams = useSearchParams();
   const filterByStatus = queryParams.get(
     "status"
   ) as keyof typeof OnboardingStatuses;
 
+  const {user} = useUser();
+
+  const isAdmin = user?.publicMetadata.role === "admin";
+
+  if (!isAdmin) {
+    redirect("/dashboard");
+  }
   return (
     <Page title="Onboarding Details">
       <OnboardingDetailsGrid filterByStatus={filterByStatus} />
