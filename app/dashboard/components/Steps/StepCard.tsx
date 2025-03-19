b34e5037ddf5305dd4ad7f8399dd2449cbb2a815
TMN <tommy.martin@adsoftware.fr>
2025-03-19 13:13:04 -0500

    fix

--- app/dashboard/components/Steps/StepCard.tsx
+++ app/dashboard/components/Steps/StepCard.tsx
@@ -1,7 +1,6 @@
 import {cn} from "@/lib/utils";
 import React from "react";
 import {ProcessStep} from "./processSteps";
-import {CircleCheck, UserCircle, Users} from "lucide-react";
 
 export const StepCard = ({
   step,
