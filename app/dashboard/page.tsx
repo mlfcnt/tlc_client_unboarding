1b34c993fa1d6f93b676cbdc8ca2c608d2281567
TMN <tommy.martin@adsoftware.fr>
2025-03-19 17:29:52 -0500

    Loading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,19 +7,31 @@ import {Title} from "./components/Title";
 import {Page} from "./components/Page";
 import {Subtitle} from "./components/Subtitle";
 import {Steps} from "./components/Steps/Steps";
+import {useOnboardRequests} from "./api/useOnboardRequests";
+import {Loader2} from "lucide-react";
 
 export default function DashboardPage() {
   const {user} = useUser();
   const userRole = user?.publicMetadata.role as keyof typeof ROLES;
+
+  const {isLoading} = useOnboardRequests();
   if (!user || !userRole || ![ROLES.admin, ROLES.sales].includes(userRole)) {
     redirect("/sign-in");
   }
 
   return (
     <Page>
-      <Title />
-      <Subtitle />
-      <Steps />
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen">
+          <Loader2 className="w-10 h-10 animate-spin" />
+        </div>
+      ) : (
+        <>
+          <Title />
+          <Subtitle />
+          <Steps />
+        </>
+      )}
     </Page>
   );
 }
